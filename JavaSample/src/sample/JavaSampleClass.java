9cb668c5b5c6ae95eb9af81d6824265caa420a63
aotaniyuuji <aotaniyuuji@localhost>
2023-08-20 16:42:24 +0900

    Java基礎学習

--- JavaSample/src/sample/JavaSampleClass.java
+++ JavaSample/src/sample/JavaSampleClass.java
@@ -5,6 +5,9 @@ public class JavaSampleClass {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello, World!");
+		int book = 800;
+		int drink = 500;
+		System.out.println(book * drink);
 	}
 
 }
