fcc7a9c4566da15989e4c61280860d1c3109d7b0
aotaniyuuji <aotaniyuuji@localhost>
2023-08-21 16:35:16 +0900

    Java基礎学習[繰り返しとブロックの省略]

--- JavaSample/src/sample/JavaSampleClass.java
+++ JavaSample/src/sample/JavaSampleClass.java
@@ -1,14 +1,18 @@
 package sample;
 
 public class JavaSampleClass {
-
 	public static void main(String[] args) {
 		boolean tenki = true;
 		if (tenki == true) {
 			System.out.println("洗濯をします");
 			System.out.println("散歩に行きます");
-		} else {
+		} else
 			System.out.println("DVDを見ます");
+		
+		boolean doorClose = true;
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
 		}
 	}
 }
