7b7ac98f908bc5218b690b6f2e777a7f52b1baf2
aotaniyuuji <aotaniyuuji@localhost>
2023-08-21 16:24:54 +0900

    Java基礎学習[条件分岐]

--- JavaSample/src/sample/JavaSampleClass.java
+++ JavaSample/src/sample/JavaSampleClass.java
@@ -3,11 +3,12 @@ package sample;
 public class JavaSampleClass {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello, World!");
-		int book = 800;
-		int drink = 500;
-		System.out.println(book * drink);
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+		}
 	}
-
 }
